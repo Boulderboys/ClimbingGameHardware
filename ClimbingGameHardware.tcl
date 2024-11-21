12e901558f2c154a45f41f611bfe5b822a26103b
MayorBosranger <lucmbos2@gmail.com>
2024-11-21 14:59:05 +0100

    Constraints update

--- ClimbingGameHardware.tcl
+++ ClimbingGameHardware.tcl
@@ -21,7 +21,7 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/src/vhdl/VgaTest.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/Board.vhd"]"\
  "[file normalize "$origin_dir/ips/clk_wiz_0/clk_wiz_0.xci"]"\
- "[file normalize "$origin_dir/vivado_project/ClimbingGameHardware.srcs/constrs_1/imports/constraints/Nexys-A7-100T-Master.xdc"]"\
+ "[file normalize "$origin_dir/src/constraints/Nexys_A7_100T-Master.xdc"]"\
   ]
   foreach ifile $files {
     if { ![file isfile $ifile] } {
@@ -221,17 +221,15 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize ${origin_dir}/vivado_project/ClimbingGameHardware.srcs/constrs_1/imports/constraints/Nexys-A7-100T-Master.xdc]"
+set file "[file normalize "$origin_dir/src/constraints/Nexys_A7_100T-Master.xdc"]"
 set file_added [add_files -norecurse -fileset $obj [list $file]]
-set file "$origin_dir/vivado_project/ClimbingGameHardware.srcs/constrs_1/imports/constraints/Nexys-A7-100T-Master.xdc"
+set file "$origin_dir/src/constraints/Nexys_A7_100T-Master.xdc"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property -name "file_type" -value "XDC" -objects $file_obj
 
 # Set 'constrs_1' fileset properties
 set obj [get_filesets constrs_1]
-set_property -name "target_constrs_file" -value "$orig_proj_dir/ClimbingGameHardware.srcs/constrs_1/imports/constraints/Nexys-A7-100T-Master.xdc" -objects $obj
-set_property -name "target_ucf" -value "$orig_proj_dir/ClimbingGameHardware.srcs/constrs_1/imports/constraints/Nexys-A7-100T-Master.xdc" -objects $obj
 
 # Create 'sim_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sim_1] ""]} {
