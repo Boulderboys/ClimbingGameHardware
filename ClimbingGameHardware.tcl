cd731fd9ecd48466fad12e47b150eb9f316c2161
Wrymouth <jacco.potters@outlook.com>
2024-12-12 15:58:28 +0100

    create DDR controller

--- ClimbingGameHardware.tcl
+++ ClimbingGameHardware.tcl
@@ -18,16 +18,6 @@
 proc checkRequiredFiles { origin_dir} {
   set status true
   set files [list \
- "[file normalize "$origin_dir/vivado_project/ClimbingGameHardware.srcs/utils_1/imports/synth_1/Board.dcp"]"\
-  ]
-  foreach ifile $files {
-    if { ![file isfile $ifile] } {
-      puts " Could not find local file $ifile "
-      set status false
-    }
-  }
-
-  set files [list \
  "[file normalize "$origin_dir/src/vhdl/VgaScreen.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/AXI_BRAM_Controller.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/VGASelector.vhd"]"\
@@ -42,12 +32,15 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/src/vhdl/OV7670/rtl/camera/ov7670_configuration.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/OV7670/rtl/vga/vga_controller.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/OV7670/top.vhd"]"\
+ "[file normalize "$origin_dir/ips/mig_7series_0/mig_7series_0.xci"]"\
+ "[file normalize "$origin_dir/src/vhdl/DDRController.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/Board.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/MicroblazeNexysWrapper.vhd"]"\
  "[file normalize "$origin_dir/src/vhdl/OV7670/ip/bram/blk_mem_gen_0.xci"]"\
  "[file normalize "$origin_dir/src/vhdl/OV7670/ip/bram_axi_lite/blk_mem_axi_lite.xci"]"\
  "[file normalize "$origin_dir/src/vhdl/OV7670/ip/bram_axi/blk_mem_axi.xci"]"\
  "[file normalize "$origin_dir/ips/clk_wiz_0/clk_wiz_0.xci"]"\
+ "[file normalize "$origin_dir/ips/mig_7series_0/mig_a.prj"]"\
  "[file normalize "$origin_dir/src/constraints/Nexys_A7_100T-Master.xdc"]"\
   ]
   foreach ifile $files {
@@ -177,13 +170,13 @@ set_property -name "simulator_language" -value "Mixed" -objects $obj
 set_property -name "sim_compile_state" -value "1" -objects $obj
 set_property -name "target_language" -value "VHDL" -objects $obj
 set_property -name "target_simulator" -value "Questa" -objects $obj
-set_property -name "webtalk.activehdl_export_sim" -value "48" -objects $obj
-set_property -name "webtalk.modelsim_export_sim" -value "48" -objects $obj
-set_property -name "webtalk.questa_export_sim" -value "48" -objects $obj
-set_property -name "webtalk.riviera_export_sim" -value "48" -objects $obj
-set_property -name "webtalk.vcs_export_sim" -value "48" -objects $obj
-set_property -name "webtalk.xcelium_export_sim" -value "3" -objects $obj
-set_property -name "webtalk.xsim_export_sim" -value "48" -objects $obj
+set_property -name "webtalk.activehdl_export_sim" -value "49" -objects $obj
+set_property -name "webtalk.modelsim_export_sim" -value "49" -objects $obj
+set_property -name "webtalk.questa_export_sim" -value "49" -objects $obj
+set_property -name "webtalk.riviera_export_sim" -value "49" -objects $obj
+set_property -name "webtalk.vcs_export_sim" -value "49" -objects $obj
+set_property -name "webtalk.xcelium_export_sim" -value "1" -objects $obj
+set_property -name "webtalk.xsim_export_sim" -value "49" -objects $obj
 set_property -name "xpm_libraries" -value "XPM_CDC XPM_FIFO XPM_MEMORY" -objects $obj
 
 # Create 'sources_1' fileset (if not found)
@@ -208,12 +201,15 @@ set files [list \
  [file normalize "${origin_dir}/src/vhdl/OV7670/rtl/camera/ov7670_configuration.vhd"] \
  [file normalize "${origin_dir}/src/vhdl/OV7670/rtl/vga/vga_controller.vhd"] \
  [file normalize "${origin_dir}/src/vhdl/OV7670/top.vhd"] \
+ [file normalize "${origin_dir}/ips/mig_7series_0/mig_7series_0.xci"] \
+ [file normalize "${origin_dir}/src/vhdl/DDRController.vhd"] \
  [file normalize "${origin_dir}/src/vhdl/Board.vhd"] \
  [file normalize "${origin_dir}/src/vhdl/MicroblazeNexysWrapper.vhd"] \
  [file normalize "${origin_dir}/src/vhdl/OV7670/ip/bram/blk_mem_gen_0.xci"] \
  [file normalize "${origin_dir}/src/vhdl/OV7670/ip/bram_axi_lite/blk_mem_axi_lite.xci"] \
  [file normalize "${origin_dir}/src/vhdl/OV7670/ip/bram_axi/blk_mem_axi.xci"] \
  [file normalize "${origin_dir}/ips/clk_wiz_0/clk_wiz_0.xci"] \
+ [file normalize "${origin_dir}/ips/mig_7series_0/mig_a.prj"] \
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -300,6 +296,20 @@ set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "VHDL" -objects $file_obj
 
+set file "$origin_dir/ips/mig_7series_0/mig_7series_0.xci"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "generate_files_for_reference" -value "0" -objects $file_obj
+set_property -name "registered_with_manager" -value "1" -objects $file_obj
+if { ![get_property "is_locked" $file_obj] } {
+  set_property -name "synth_checkpoint_mode" -value "Singular" -objects $file_obj
+}
+
+set file "$origin_dir/src/vhdl/DDRController.vhd"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "file_type" -value "VHDL" -objects $file_obj
+
 set file "$origin_dir/src/vhdl/Board.vhd"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
@@ -345,6 +355,11 @@ set_property -name "registered_with_manager" -value "1" -objects $file_obj
 if { ![get_property "is_locked" $file_obj] } {
   set_property -name "synth_checkpoint_mode" -value "Singular" -objects $file_obj
 }
+
+set file "$origin_dir/ips/mig_7series_0/mig_a.prj"
+set file [file normalize $file]
+set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
+set_property -name "scoped_to_cells" -value "mig_7series_0" -objects $file_obj
 
 
 # Set 'sources_1' fileset file properties for local files
@@ -392,20 +407,7 @@ set_property -name "top_lib" -value "xil_defaultlib" -objects $obj
 
 # Set 'utils_1' fileset object
 set obj [get_filesets utils_1]
-# Add local files from the original project (-no_copy_sources specified)
-set files [list \
- [file normalize "${origin_dir}/vivado_project/ClimbingGameHardware.srcs/utils_1/imports/synth_1/Board.dcp" ]\
-]
-set added_files [add_files -fileset utils_1 $files]
-
-# Set 'utils_1' fileset file properties for remote files
-# None
-
-# Set 'utils_1' fileset file properties for local files
-set file "synth_1/Board.dcp"
-set file_obj [get_files -of_objects [get_filesets utils_1] [list "*$file"]]
-set_property -name "netlist_only" -value "0" -objects $file_obj
-
+# Empty (no sources present)
 
 # Set 'utils_1' fileset properties
 set obj [get_filesets utils_1]
@@ -454,13 +456,19 @@ if { [get_files vga_controller.vhd] == "" } {
 if { [get_files top.vhd] == "" } {
   import_files -quiet -fileset sources_1 "$origin_dir/src/vhdl/OV7670/top.vhd"
 }
+if { [get_files mig_7series_0.xci] == "" } {
+  import_files -quiet -fileset sources_1 "$origin_dir/ips/mig_7series_0/mig_7series_0.xci"
+}
+if { [get_files DDRController.vhd] == "" } {
+  import_files -quiet -fileset sources_1 "$origin_dir/src/vhdl/DDRController.vhd"
+}
 
 
 # Proc to create BD bd_microblaze
 proc cr_bd_bd_microblaze { parentCell } {
 # The design that will be created by this Tcl proc contains the following 
 # module references:
-# VgaScreen, AXI_BRAM_Controller, VGASelector, OV7670Top
+# VgaScreen, AXI_BRAM_Controller, VGASelector, OV7670Top, DDRController
 
 
 
@@ -518,6 +526,7 @@ proc cr_bd_bd_microblaze { parentCell } {
   AXI_BRAM_Controller\
   VGASelector\
   OV7670Top\
+  DDRController\
   "
 
    set list_mods_missing ""
@@ -686,6 +695,20 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
   set vga_used_by_camera_0 [ create_bd_port -dir I vga_used_by_camera_0 ]
   set ov7670_reset_0 [ create_bd_port -dir O -type rst ov7670_reset_0 ]
   set frame_buffer_out_processing_0 [ create_bd_port -dir O -from 11 -to 0 frame_buffer_out_processing_0 ]
+  set ddr2_ras_n_0 [ create_bd_port -dir O ddr2_ras_n_0 ]
+  set ddr2_cs_n_0 [ create_bd_port -dir O -from 0 -to 0 ddr2_cs_n_0 ]
+  set ddr2_ck_p_0 [ create_bd_port -dir O -from 0 -to 0 ddr2_ck_p_0 ]
+  set ddr2_ck_n_0 [ create_bd_port -dir O -from 0 -to 0 ddr2_ck_n_0 ]
+  set ddr2_ba_0 [ create_bd_port -dir O -from 2 -to 0 ddr2_ba_0 ]
+  set ddr2_dq_0 [ create_bd_port -dir IO -from 15 -to 0 ddr2_dq_0 ]
+  set ddr2_dqs_p_0 [ create_bd_port -dir IO -from 1 -to 0 ddr2_dqs_p_0 ]
+  set ddr2_dm_0 [ create_bd_port -dir O -from 1 -to 0 ddr2_dm_0 ]
+  set ddr2_odt_0 [ create_bd_port -dir O -from 0 -to 0 ddr2_odt_0 ]
+  set ddr2_cas_n_0 [ create_bd_port -dir O ddr2_cas_n_0 ]
+  set ddr2_we_n_0 [ create_bd_port -dir O ddr2_we_n_0 ]
+  set ddr2_addr_0 [ create_bd_port -dir O -from 12 -to 0 ddr2_addr_0 ]
+  set ddr2_cke_0 [ create_bd_port -dir O -from 0 -to 0 ddr2_cke_0 ]
+  set ddr2_dqs_n_0 [ create_bd_port -dir IO -from 1 -to 0 ddr2_dqs_n_0 ]
 
   # Create instance: microblaze_riscv_0, and set properties
   set microblaze_riscv_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:microblaze_riscv:1.0 microblaze_riscv_0 ]
@@ -707,19 +730,23 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
   set clk_wiz_1 [ create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz_1 ]
   set_property -dict [list \
     CONFIG.CLKOUT1_DRIVES {BUFG} \
-    CONFIG.CLKOUT1_JITTER {137.681} \
-    CONFIG.CLKOUT1_PHASE_ERROR {105.461} \
+    CONFIG.CLKOUT1_JITTER {130.958} \
+    CONFIG.CLKOUT1_PHASE_ERROR {98.575} \
     CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {100} \
     CONFIG.CLKOUT2_DRIVES {BUFG} \
-    CONFIG.CLKOUT2_JITTER {183.467} \
-    CONFIG.CLKOUT2_PHASE_ERROR {105.461} \
+    CONFIG.CLKOUT2_JITTER {175.402} \
+    CONFIG.CLKOUT2_PHASE_ERROR {98.575} \
     CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {25} \
     CONFIG.CLKOUT2_USED {true} \
     CONFIG.CLKOUT3_DRIVES {BUFG} \
-    CONFIG.CLKOUT3_JITTER {137.681} \
-    CONFIG.CLKOUT3_PHASE_ERROR {105.461} \
+    CONFIG.CLKOUT3_JITTER {130.958} \
+    CONFIG.CLKOUT3_PHASE_ERROR {98.575} \
     CONFIG.CLKOUT3_USED {true} \
     CONFIG.CLKOUT4_DRIVES {BUFG} \
+    CONFIG.CLKOUT4_JITTER {114.829} \
+    CONFIG.CLKOUT4_PHASE_ERROR {98.575} \
+    CONFIG.CLKOUT4_REQUESTED_OUT_FREQ {200.000} \
+    CONFIG.CLKOUT4_USED {true} \
     CONFIG.CLKOUT5_DRIVES {BUFG} \
     CONFIG.CLKOUT6_DRIVES {BUFG} \
     CONFIG.CLKOUT7_DRIVES {BUFG} \
@@ -727,13 +754,14 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
     CONFIG.CLK_IN2_BOARD_INTERFACE {Custom} \
     CONFIG.JITTER_SEL {No_Jitter} \
     CONFIG.MMCM_BANDWIDTH {OPTIMIZED} \
-    CONFIG.MMCM_CLKFBOUT_MULT_F {9} \
-    CONFIG.MMCM_CLKOUT0_DIVIDE_F {9} \
-    CONFIG.MMCM_CLKOUT1_DIVIDE {36} \
-    CONFIG.MMCM_CLKOUT2_DIVIDE {9} \
+    CONFIG.MMCM_CLKFBOUT_MULT_F {10} \
+    CONFIG.MMCM_CLKOUT0_DIVIDE_F {10} \
+    CONFIG.MMCM_CLKOUT1_DIVIDE {40} \
+    CONFIG.MMCM_CLKOUT2_DIVIDE {10} \
+    CONFIG.MMCM_CLKOUT3_DIVIDE {5} \
     CONFIG.MMCM_COMPENSATION {ZHOLD} \
     CONFIG.MMCM_DIVCLK_DIVIDE {1} \
-    CONFIG.NUM_OUT_CLKS {3} \
+    CONFIG.NUM_OUT_CLKS {4} \
     CONFIG.PRIMITIVE {PLL} \
     CONFIG.PRIM_SOURCE {Single_ended_clock_capable_pin} \
     CONFIG.RESET_BOARD_INTERFACE {reset} \
@@ -848,6 +876,17 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
      return 1
    }
   
+  # Create instance: DDRController_0, and set properties
+  set block_name DDRController
+  set block_cell_name DDRController_0
+  if { [catch {set DDRController_0 [create_bd_cell -type module -reference $block_name $block_cell_name] } errmsg] } {
+     catch {common::send_gid_msg -ssname BD::TCL -id 2095 -severity "ERROR" "Unable to add referenced block <$block_name>. Please add the files for ${block_name}'s definition into the project."}
+     return 1
+   } elseif { $DDRController_0 eq "" } {
+     catch {common::send_gid_msg -ssname BD::TCL -id 2096 -severity "ERROR" "Unable to referenced block <$block_name>. Please add the files for ${block_name}'s definition into the project."}
+     return 1
+   }
+  
   # Create interface connections
   connect_bd_intf_net -intf_net axi_gpio_0_GPIO [get_bd_intf_ports led_16bits] [get_bd_intf_pins axi_gpio_0/GPIO]
   connect_bd_intf_net -intf_net axi_gpio_1_GPIO [get_bd_intf_ports dip_switches_16bits] [get_bd_intf_pins axi_gpio_1/GPIO]
@@ -866,8 +905,22 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
   connect_bd_net -net AXI_BRAM_Controller_0_bram_clk [get_bd_pins AXI_BRAM_Controller_0/bram_clk] [get_bd_pins blk_mem_gen_0/clka]
   connect_bd_net -net AXI_BRAM_Controller_0_bram_din [get_bd_pins AXI_BRAM_Controller_0/bram_din] [get_bd_pins blk_mem_gen_0/dina]
   connect_bd_net -net AXI_BRAM_Controller_0_bram_we [get_bd_pins AXI_BRAM_Controller_0/bram_we] [get_bd_pins blk_mem_gen_0/wea]
+  connect_bd_net -net DDRController_0_ddr2_addr [get_bd_pins DDRController_0/ddr2_addr] [get_bd_ports ddr2_addr_0]
+  connect_bd_net -net DDRController_0_ddr2_ba [get_bd_pins DDRController_0/ddr2_ba] [get_bd_ports ddr2_ba_0]
+  connect_bd_net -net DDRController_0_ddr2_cas_n [get_bd_pins DDRController_0/ddr2_cas_n] [get_bd_ports ddr2_cas_n_0]
+  connect_bd_net -net DDRController_0_ddr2_ck_n [get_bd_pins DDRController_0/ddr2_ck_n] [get_bd_ports ddr2_ck_n_0]
+  connect_bd_net -net DDRController_0_ddr2_ck_p [get_bd_pins DDRController_0/ddr2_ck_p] [get_bd_ports ddr2_ck_p_0]
+  connect_bd_net -net DDRController_0_ddr2_cke [get_bd_pins DDRController_0/ddr2_cke] [get_bd_ports ddr2_cke_0]
+  connect_bd_net -net DDRController_0_ddr2_cs_n [get_bd_pins DDRController_0/ddr2_cs_n] [get_bd_ports ddr2_cs_n_0]
+  connect_bd_net -net DDRController_0_ddr2_dm [get_bd_pins DDRController_0/ddr2_dm] [get_bd_ports ddr2_dm_0]
+  connect_bd_net -net DDRController_0_ddr2_odt [get_bd_pins DDRController_0/ddr2_odt] [get_bd_ports ddr2_odt_0]
+  connect_bd_net -net DDRController_0_ddr2_ras_n [get_bd_pins DDRController_0/ddr2_ras_n] [get_bd_ports ddr2_ras_n_0]
+  connect_bd_net -net DDRController_0_ddr2_we_n [get_bd_pins DDRController_0/ddr2_we_n] [get_bd_ports ddr2_we_n_0]
   connect_bd_net -net Net [get_bd_ports scl_0] [get_bd_pins OV7670Top_0/scl]
   connect_bd_net -net Net1 [get_bd_ports sda_0] [get_bd_pins OV7670Top_0/sda]
+  connect_bd_net -net Net2 [get_bd_ports ddr2_dq_0] [get_bd_pins DDRController_0/ddr2_dq]
+  connect_bd_net -net Net3 [get_bd_ports ddr2_dqs_p_0] [get_bd_pins DDRController_0/ddr2_dqs_p]
+  connect_bd_net -net Net4 [get_bd_ports ddr2_dqs_n_0] [get_bd_pins DDRController_0/ddr2_dqs_n]
   connect_bd_net -net OV7670Top_0_VGA_B [get_bd_pins OV7670Top_0/VGA_B] [get_bd_pins VGASelector_0/vga_b_camera]
   connect_bd_net -net OV7670Top_0_VGA_G [get_bd_pins OV7670Top_0/VGA_G] [get_bd_pins VGASelector_0/vga_g_camera]
   connect_bd_net -net OV7670Top_0_VGA_HS_O [get_bd_pins OV7670Top_0/VGA_HS_O] [get_bd_pins VGASelector_0/vga_hs_camera]
@@ -893,6 +946,7 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
   connect_bd_net -net clk_wiz_1_clk_out1 [get_bd_pins clk_wiz_1/clk_out1] [get_bd_pins smartconnect_0/aclk] [get_bd_pins axi_gpio_0/s_axi_aclk] [get_bd_pins axi_gpio_1/s_axi_aclk] [get_bd_pins axi_uartlite_0/s_axi_aclk] [get_bd_pins microblaze_riscv_0/Clk] [get_bd_pins microblaze_riscv_0_local_memory/LMB_Clk] [get_bd_pins rst_clk_wiz_1_100M/slowest_sync_clk] [get_bd_pins blk_mem_gen_0/clkb] [get_bd_pins AXI_BRAM_Controller_0/s_axi_aclk]
   connect_bd_net -net clk_wiz_1_clk_out2 [get_bd_pins clk_wiz_1/clk_out2] [get_bd_pins VgaScreen_0/clk]
   connect_bd_net -net clk_wiz_1_clk_out3 [get_bd_pins clk_wiz_1/clk_out3] [get_bd_pins OV7670Top_0/clk]
+  connect_bd_net -net clk_wiz_1_clk_out4 [get_bd_pins clk_wiz_1/clk_out4] [get_bd_pins DDRController_0/clk_200MHz_i]
   connect_bd_net -net clk_wiz_1_locked [get_bd_pins clk_wiz_1/locked] [get_bd_pins rst_clk_wiz_1_100M/dcm_locked]
   connect_bd_net -net mdm_1_debug_sys_rst [get_bd_pins mdm_1/Debug_SYS_Rst] [get_bd_pins rst_clk_wiz_1_100M/mb_debug_sys_rst]
   connect_bd_net -net ov7670_data_0_1 [get_bd_ports ov7670_data_0] [get_bd_pins OV7670Top_0/ov7670_data]
@@ -900,7 +954,7 @@ proc create_hier_cell_microblaze_riscv_0_local_memory { parentCell nameHier } {
   connect_bd_net -net ov7670_pclk_0_1 [get_bd_ports ov7670_pclk_0] [get_bd_pins OV7670Top_0/ov7670_pclk]
   connect_bd_net -net ov7670_vsync_0_1 [get_bd_ports ov7670_vsync_0] [get_bd_pins OV7670Top_0/ov7670_vsync]
   connect_bd_net -net reset_1 [get_bd_ports reset] [get_bd_pins reset_inv_0/Op1] [get_bd_pins rst_clk_wiz_1_100M/ext_reset_in]
-  connect_bd_net -net reset_inv_0_Res [get_bd_pins reset_inv_0/Res] [get_bd_pins clk_wiz_1/reset]
+  connect_bd_net -net reset_inv_0_Res [get_bd_pins reset_inv_0/Res] [get_bd_pins clk_wiz_1/reset] [get_bd_pins DDRController_0/rst_i]
   connect_bd_net -net rst_clk_wiz_1_100M_bus_struct_reset [get_bd_pins rst_clk_wiz_1_100M/bus_struct_reset] [get_bd_pins microblaze_riscv_0_local_memory/SYS_Rst]
   connect_bd_net -net rst_clk_wiz_1_100M_mb_reset [get_bd_pins rst_clk_wiz_1_100M/mb_reset] [get_bd_pins microblaze_riscv_0/Reset]
   connect_bd_net -net rst_clk_wiz_1_100M_peripheral_aresetn [get_bd_pins rst_clk_wiz_1_100M/peripheral_aresetn] [get_bd_pins axi_uartlite_0/s_axi_aresetn] [get_bd_pins axi_gpio_0/s_axi_aresetn] [get_bd_pins axi_gpio_1/s_axi_aresetn] [get_bd_pins smartconnect_0/aresetn] [get_bd_pins AXI_BRAM_Controller_0/s_axi_aresetn]
@@ -959,7 +1013,7 @@ if { $obj != "" } {
 
 }
 set obj [get_runs synth_1]
-set_property -name "incremental_checkpoint" -value "$proj_dir/ClimbingGameHardware.srcs/utils_1/imports/synth_1/Board.dcp" -objects $obj
+set_property -name "needs_refresh" -value "1" -objects $obj
 set_property -name "auto_incremental_checkpoint" -value "1" -objects $obj
 set_property -name "strategy" -value "Vivado Synthesis Defaults" -objects $obj
 
@@ -1182,6 +1236,7 @@ set_property -name "options.warn_on_violation" -value "1" -objects $obj
 
 }
 set obj [get_runs impl_1]
+set_property -name "needs_refresh" -value "1" -objects $obj
 set_property -name "strategy" -value "Vivado Implementation Defaults" -objects $obj
 set_property -name "steps.write_bitstream.args.readback_file" -value "0" -objects $obj
 set_property -name "steps.write_bitstream.args.verbose" -value "0" -objects $obj
